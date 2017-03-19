7d773476a6c847bf6ea51c6eb5e21a7916ebb6cd
SSH <shenshaohui1991@gmail.com>
2017-03-19 17:38:50 +0800

    删除配置文件

--- app/js/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Tea on 2017/2/28.
- */
-module.exports = {
-    imgURL: 'http://10.27.95.39:8080/fileService/file/upload',
-    staticURL: 'http://118.178.185.71:3000/upload',
-    overseaURL: ''
-};
